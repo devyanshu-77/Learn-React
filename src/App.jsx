b5e8b9e30f910e2799c193b2161e0ff2ea66ad6a
Divyanshu <divyanshu473b@gmail.com>
2026-02-04 22:26:32 +0530

    learned about JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 function App() {
+  const name = "John Doe"
+  const heroNames = ["SpiderMan", "Hulk", "Thor", "Captain America"]
+  const isLoggedIn = true;
+  const styles = {
+      color: "red",
+      fontSize: "55px"
+  }
   return (
     <>
-      <h1>Hello React </h1>
+      <h1 className="text-2xl">Hello React </h1>
+      <p style={styles}>Hello {name} </p>
+      <ul>
+        {heroNames.map((name, i) => {
+           return <li key={i}>Hi I am {name} </li>
+        })}
+      </ul>
+      <p>You are {isLoggedIn ? "logged in." : "not logged in."} </p>
     </>
   )
 }
